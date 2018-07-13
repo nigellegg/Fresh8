857d253b97d4194eee79f6ea04f06d37a8a51662
Nigel Legg <nigel.legg@freemavens.com>
2018-07-13 06:29:51 +0100

    created class structure

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 There are two modules to be built: 
 1.  Data Generation and "streaming". 
 2.  Data ingestion. 
+
+Data Generation - gen.py
+called using python gen.py and 
--- gen.py
+++ gen.py
@@ -1,13 +1,35 @@
+import sys
 import uuid
 from datetime import datetime
 import json
 view = uuid.uuid1()
 
-viewID = uuid.uuid1() 
+'''viewID = uuid.uuid1() 
 item1 = "viewID:"
-json1 = {"type": "Viewed", "data":{"viewID": str(uuid.uuid1()), "eventDateTime": str(datetime.now())}}
+
 json.dumps(json1)
 json2 = {"type": "Interacted", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
 json.dumps(json2)
 print(json1)
 print(json2)
+'''
+
+class Generate():
+
+	def __init__(self):
+	    self.data = '' 
+	    self.groups = int(sys.argv[1])
+	    self.batch = int(sys.argv[2])
+	    self.interval = int(sys.argv[3])
+	    self.output = sys.argv[4]
+	
+	def create_view(self):
+		groups = self.groups
+		print('Need to create '+str(groups)+' groups.')
+        json1 = {"type": "Viewed", "data":{"viewID": str(uuid.uuid1()), "eventDateTime": str(datetime.now())}}
+
+
+
+if __name__ == "__main__":
+	gen_data = Generate()
+	gen_data.create_view()
