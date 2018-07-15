4ebccf149dda14828ad54e5ceaedd0cce20b6889
Nigel Legg <nigel.legg@freemavens.com>
2018-07-15 08:42:20 +0100

    timertest and timer add

--- gen.py
+++ gen.py
@@ -2,7 +2,8 @@ import sys
 import uuid
 from datetime import datetime
 import json
-view = uuid.uuid1()
+#view = uuid.uuid1()
+import time
 
 '''viewID = uuid.uuid1() 
 item1 = "viewID:"
@@ -16,18 +17,22 @@ print(json2)
 
 class Generate():
 
-	def __init__(self):
-	    self.data = '' 
-	    self.groups = int(sys.argv[1])
-	    self.batch = int(sys.argv[2])
-	    self.interval = int(sys.argv[3])
-	    self.output = sys.argv[4]
+    def __init__(self):
+        self.data = '' 
+        self.groups = int(sys.argv[1])
+        self.batch = int(sys.argv[2])
+        interval = int(sys.argv[3])
+        self.output = sys.argv[4]
+        starttime=time.time()
+        while True:
+            self.checknsave()
+            time.sleep(interval - ((time.time() - starttime) % interval))
 	
-	def create_view(self):
-		groups = self.groups
-		print('Need to create '+str(groups)+' groups.')
-		i = 0
-		while i<groups:
+    def create_data(self):
+        groups = self.groups
+        print('Need to create '+str(groups)+' groups.')
+        i = 0
+        while i<groups:
             json1 = {"type": "Viewed", "data":{"viewID": str(uuid.uuid1()), "eventDateTime": str(datetime.now())}}
             self.data = self.data+json1+'\n'
             if i % 20 == 0:
@@ -37,13 +42,18 @@ class Generate():
                 json3 = {"type": "Click Through", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
                 self.data = self.data+json3+'\n'
             if i % 20 == 2:
-
+                json4 = {"type":  "Interacted", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
+                json5 = {"type": "Click Through", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
+                self.data = self.data+json4+'\n'
+                self.data = self.data+json5+'\n'
             i += 1
 
-    def create_interaction(self):
+    def checknsave(self):
+        print("Check size of data and save... ")
+
 
 
 
 if __name__ == "__main__":
 	gen_data = Generate()
-	gen_data.create_view()
+	gen_data.create_data()
