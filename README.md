6504912b6d639055166f4dce9bf6fcd273d1f462
Nigel Legg <nigel.legg@freemavens.com>
2018-07-18 04:52:05 +0100

    checknsave complete

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ There are two modules to be built:
 2.  Data ingestion. 
 
 Data Generation - gen.py
-called using python gen.py and 
+called using python gen.py with the following command line arguments:
+	- number of groups (integer) - number of event groups created
+	- batch-size (integer) - number of event groups per output file
+	- interval (integer) - interval in seconds between each file creation.
+	- output directory (string) - where files are saved to
+
+
--- gen.py
+++ gen.py
@@ -2,31 +2,21 @@ import sys
 import uuid
 from datetime import datetime
 import json
-#view = uuid.uuid1()
 import time
 
-'''viewID = uuid.uuid1() 
-item1 = "viewID:"
-
-json.dumps(json1)
-json2 = {"type": "Interacted", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
-json.dumps(json2)
-print(json1)
-print(json2)
-'''
 
 class Generate():
 
     def __init__(self):
-        self.data = '' 
+        self.data = []
         self.groups = int(sys.argv[1])
         self.batch = int(sys.argv[2])
         interval = int(sys.argv[3])
         self.output = sys.argv[4]
-        starttime=time.time()
-        while True:
-            self.checknsave()
-            time.sleep(interval - ((time.time() - starttime) % interval))
+        #starttime=time.time()
+        #while True:
+        #    self.checknsave()
+        #    time.sleep(interval - ((time.time() - starttime) % interval))
 	
     def create_data(self):
         groups = self.groups
@@ -34,24 +24,45 @@ class Generate():
         i = 0
         while i<groups:
             json1 = {"type": "Viewed", "data":{"viewID": str(uuid.uuid1()), "eventDateTime": str(datetime.now())}}
-            self.data = self.data+json1+'\n'
+            self.data.append(json1)
             if i % 20 == 0:
                 json2 = {"type": "Interacted", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
-                self.data = self.data+json2+'\n'
-            if i % 20 == 1: 
+                self.data.append(json2) 
+            if i % 2 == 1: 
                 json3 = {"type": "Click Through", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
-                self.data = self.data+json3+'\n'
+                self.data.append(json3)
             if i % 20 == 2:
                 json4 = {"type":  "Interacted", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
                 json5 = {"type": "Click Through", "data":{"viewID": json1['data']['viewID'], "eventDateTime": str(datetime.now())}}
-                self.data = self.data+json4+'\n'
-                self.data = self.data+json5+'\n'
+                self.data.append(json4)
+                self.data.append(json5)
             i += 1
+        print(self.data)
+        out = open(self.output+'data.txt', 'w')
+        out.write(self.data)
 
+        
     def checknsave(self):
         print("Check size of data and save... ")
-
-
+        data = self.data
+        djson = json.dumps(data)
+        views = []
+        interact = []
+        click = []
+        out = open('events'+datetime.now()+'.json')
+        while len(views) < self.batch:
+            for x in djson:
+                if x['type'] == 'Viewed':
+                    views.append(x['data']['viewID'])
+                if x['type'] == 'Interacted':
+                    interact.append(x['data']['viewID'])
+                if x['type'] == 'Click Through':
+                    click.append(x['data']['viewID'])
+        for y in djson:
+            if y['data']['viewID'] in views:
+                out.write(str(y))
+        out.close()
+            
 
 
 if __name__ == "__main__":
