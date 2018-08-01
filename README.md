993024afaf536de279fb61080f365473a5df37b7
Nigel Legg <nigel.legg@freemavens.com>
2018-08-01 06:51:55 +0100

    final

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ There are two modules to be built:
 1.  Data Generation and "streaming". 
 2.  Data ingestion. 
 
-Data Generation - gendata.py
+There are two scripts, written and run in Python3.  
+I attempted a first pass in Go, but it was taking far too long to work out.  That might
+have been a better option for this task.  I also attempted to put this on Google Cloud, 
+but had problems installing the SDK, so it is a totally offline app.   
+
+1. Data Generation - gendata.py
 called using python gen.py with the following command line arguments:
 	- number of groups (integer) - number of event groups created
 	- batch-size (integer) - number of event groups per output file
@@ -18,6 +23,10 @@ Run from a command line, eg
 	
 Threading is used to run the data creation and file save functions in parallel
 within this single script.    
+
+2. Data ingestion - read_use.py
+Collection and counting script
+read_use.py: run using python3 read_use.py 5, where the argument, 5, is the time interval between checks.  
 
 
 
--- read_use.py
+++ read_use.py
@@ -7,14 +7,14 @@ import os
 class ReadUse():
 
     def __init__(self):
-    	self.interval = int(sys.argv[1])
-    	self.views = 0
-    	self.interact = 0
-    	self.click = 0
-    	self.interclick = 0
+        self.interval = int(sys.argv[1])
+        self.views = 0
+        self.interact = 0
+        self.click = 0
+        self.interclick = 0
 
     def read(self):
-    	files = os.listdir('./data')
+        files = os.listdir('./data')
         now = datetime.now()
         diff = []
         for f in files: 
@@ -23,42 +23,43 @@ class ReadUse():
         fuse = diff.index(min(diff))
         usef = files[fuse]
         text = open(usef)
+        
         cviews = 0
-        interact = 0
-        click = 0
+        cinteract = 0
+        cclick = 0
         interclick = 0 
+        inter = 0
+        
         for line in text:
             line = json.dumps(line)
             if line['type'] ==  'Viewed':
                 cviews += 1
-        self.views = cviews
-    	self.interact = cinteract
-    	self.click = cclick
-    	self.interclick = cinterclick
-    	return True
+                vid = x['data']['viewID']
+            if line['type'] == 'Interaction':
+                cinteract += 1
+                inter = 1
+            if line['type'] == 'Click' and inter == 0:
+                cclick += 1
+            if line['type'] == 'Click' and inter == 1:
+                cinterclick += 1 
+        
+        print('Views', str(cviews))
+        print('Interact', str(cinteract))
+        print('Click through', cclick)
+        print('interact and click through', cinterclick)
 
-    def use(self):
-    	views = self.views
-    	interact = self.interact
-    	click = self.click
-    	interclick = self.interclick
-    	return True
+        return True
 
 
     def run(self):
-    	t1 = threading.Thread(target=self.read)
-    	t2 = threading.Thread(target=self.use)
+        t1 = threading.Thread(target=self.read)
         interval = self.interval
         while True:
             t1.start()
-            time.sleep(interval - ((time.time() - starttime) % interval))
-    	while True:
-    	    t2.start()
-            time.sleep(interval - ((time.time() - starttime) % interval))
+            time.sleep(interval - (int((datetime.now()-starttime).seconds) % interval))
 
 
 if __name__ == '__main__':
-	starttime = datetime.now()
-	ru = ReadUse()
-	ru.read()
-	ru.use()
+    starttime = datetime.now()
+    ru = ReadUse()
+    ru.run()
